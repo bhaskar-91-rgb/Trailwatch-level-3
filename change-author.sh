9268283493419451cd17cb4fe4c677f1d10d6814
bhaskar-91-rgb <ronnybhaskar54@gmail.com>
2026-07-14 20:48:02 +0530

    Fix files

---
+++ change-author.sh
@@ -0,0 +1,4 @@
+export GIT_AUTHOR_NAME="bhaskar-91-rgb"
+export GIT_AUTHOR_EMAIL="ronnybhaskar54@gmail.com"
+export GIT_COMMITTER_NAME="bhaskar-91-rgb"
+export GIT_COMMITTER_EMAIL="ronnybhaskar54@gmail.com"
